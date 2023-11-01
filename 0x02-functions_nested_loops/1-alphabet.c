2a6ccc504751520e46b053821a68619d3327dcee
Hillary <hillaryowino44@gmail.com>
2023-11-01 11:09:34 -0700

    1-main.c, 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_the_alphabet - In lowercase.
+ *
+ * return:void
+ */
+
+void print_the_alphabet(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
