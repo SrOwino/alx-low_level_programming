5922a1392696591551bb1628e08513dfc3579b48
Hillary <hillaryowino44@gmail.com>
2023-11-13 07:51:05 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,7 +11,6 @@
 char *_strpbrk(char *s, char *accept)
 {
 int i, j;
-
 for (i = 0; s[i] != '\0'; i++)
 {
 for (j = 0; accept[j] != '\0'; j++)
@@ -20,6 +19,5 @@ if (s[i] == accept[j])
 return &s[i];
 }
 }
-
 return (NULL);
 }
