7cdd1f9a8f58ce6cbe8fbdeb74dadfeba5c0041a
Hillary <hillaryowino44@gmail.com>
2023-11-12 21:28:31 -0800

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: Pointer to the string.
+ * @c: Character to locate.
+ *
+ * Return: Pointer to the first occurrence of the character c else NULL.
+ */
+char *_strchr(char *s, char c)
+{
+while (*s != '\0')
+{
+if (*s == c)
+return s;
+s++;
+}
+
+if (c == '\0')
+return s;
+
+return (NULL);
+}
